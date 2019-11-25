83207e933a2f103656138b6578432c0a496b53b2
Bruno Pontes <brunocrpontes@gmail.com>
2019-11-25 19:06:26 -0300

    changes

--- source/models/payment.js
+++ source/models/payment.js
@@ -1,15 +1,15 @@
 const { Schema, model } = require('mongoose')
 
-const paymentSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  value: { type: Number, required: true },
-  remember: { type: Boolean, required: true },
-  rememberFrequency: { type: String, enum: ['Daily', 'Weekly', 'Yearly' ], required: () => this.remember },
-  date: { type: String, required: true, validate: /(\d{2})\/(\d{2})\/(\d{4})/ },
-  category: {type: Schema.Types.ObjectId, ref: 'Category', required: true}
-}, {
-  timestamps: true
-})
+const paymentSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    value: { type: Number, required: true },
+    remember: { type: Boolean, required: true }
+  },
+  {
+    timestamps: true
+  }
+)
 
 module.exports = model('Payment', paymentSchema)
